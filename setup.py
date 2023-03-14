be377d897644663148429f77217d9b92b74879ce
Kyle Davis <25229537+dev-KyleDavis@users.noreply.github.com>
2023-03-14 00:40:58 +0000

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from cx_Freeze import setup, Executable
-
-setup(name='enter name of program here',
-      version='1.0',
-      description='By: K. Davis 2023',
-      executables=[Executable('python file name here.py')])
-
-# run this command in the cmd, in the same dir as the python program - "python setup.py build"
